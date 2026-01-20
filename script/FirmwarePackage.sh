cf29372d374faf02d48de9b7cb408df8569b66f9
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2026-01-20 16:26:56 +0800

    0

--- script/FirmwarePackage.sh
+++ script/FirmwarePackage.sh
@@ -11,11 +11,8 @@ echo ''
 
 # --------------------------------------------------
 
-# repoUrl='https://github.com/zero-dream/zerowrt-repository/releases/latest/download'
-
 # function addPackage() {
 # 	local name=$1
-# 	curl -L "$repoUrl/$package.tar.gz" | tar -xzv -C "$WRT_MainPath/package/"
 # }
 
 # packages=(
@@ -23,7 +20,9 @@ echo ''
 # )
 # repoUrl='https://github.com/zero-dream/zerowrt-repository/releases/latest/download'
 # for package in "${packages[@]}"; do
-# 	curl -L "$repoUrl/$package.tar.gz" | tar -xzv -C "$WRT_MainPath/package/"
+# 	pkgPath="$WRT_MainPath/package/zerowrt"
+# 	mkdir -p "$pkgPath"
+# 	curl -L "$repoUrl/$package.tar.gz" | tar -xz -C "$pkgPath"
 # done
 
 # --------------------------------------------------
@@ -68,9 +67,9 @@ UPDATE_PACKAGE() {
 # 这样会把原有的 open-app-filter, luci-app-appfilter, oaf 相关组件删除，不会出现 coremark 错误
 
 # UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-25.12"
-UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
-UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
-UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
+# UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
+# UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
+# UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"
 UPDATE_PACKAGE "netspeedtest" "sirpdboy/luci-app-netspeedtest" "master" "" "homebox speedtest"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
