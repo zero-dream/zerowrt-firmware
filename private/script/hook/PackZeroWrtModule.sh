a6a4391112b057f494e744ef23f0e79405e84d01
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2026-03-17 21:38:55 +0800

    Add kernel module zip

--- private/script/hook/PackZeroWrtModule.sh
+++ private/script/hook/PackZeroWrtModule.sh
@@ -18,11 +18,22 @@ source "$ZD_LibPath/private/getApp.sh"
 # --------------------------------------------------
 
 # PackZeroWrtModule
-outputPath="$CI_TempPath/PackModule" && mkdir -p "$outputPath"
+outputPath="$CI_TempPath/ZeroWrtModule" && mkdir -p "$outputPath"
 appPath=$(getApp 'zerowrt-virtual-linux-amd64') || exit 1
 "$appPath" packmodule \
+  --isEncrypt=true \
   --dataPath "$WRT_MainPath/bin" \
   --configPath "$WRT_ConfigPath" \
+  --outputPath "$outputPath"
+[[ $? -ne 0 ]] && exit 1
+
+# PackKernelModule
+outputPath="$CI_TempPath/KernelModule" && mkdir -p "$outputPath"
+appPath=$(getApp 'zerowrt-virtual-linux-amd64') || exit 1
+"$appPath" packmodule \
+  --isEncrypt=false \
+  --dataPath "$WRT_MainPath/bin" \
+  --configPath "$CI_VirtualPath/VirtualConfig" \
   --outputPath "$outputPath"
 [[ $? -ne 0 ]] && exit 1
 
